60a2acc5c62bdb185ce8d468efd710ff6b4dd246
Víctor Ramírez de la Corte <virako.9@gmail.com>
2021-07-22 22:58:49 +0200

    Fix hint typing error

--- tests/models/test_game.py
+++ tests/models/test_game.py
@@ -5,14 +5,14 @@ from src.models.game import Game
 
 class GameTest(unittest.TestCase):
 
-    def setUp(self):
+    def setUp(self) -> None:
         self.game = Game()
 
-    def test_initial_game_have_two_player(self):
+    def test_initial_game_have_two_player(self) -> None:
         self.assertIsNotNone(self.game.red_player)
         self.assertIsNotNone(self.game.black_player)
 
-    def test_initial_game_have_turn_none(self):
+    def test_initial_game_have_turn_none(self) -> None:
         self.assertIsNone(self.game.turn)
 
 
