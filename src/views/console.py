8d08742e845f99d3d77cd91fc999d8436f75190e
victor <hugohg34@gmail.com>
2021-08-07 00:35:55 +0200

    console implementation

--- src/views/console.py
+++ src/views/console.py
@@ -1,6 +1,28 @@
-def show(msg: str = '') -> None:
-    print(msg)
+from typing import List
 
+class Console:
+    def show(self, msg: str) -> None:
+        print(msg)
+            
+    def read_str(self, msg: str = '') -> str:
+        while True:
+            user_input: str = input(msg + ': ')
+            if user_input:
+                break
+        return user_input
 
-def read_str(msg: str = '') -> str:
-    return msg
+    def read_int(self, msg: str = '') -> int:
+        user_str: str = self.read_str(msg)
+        try:
+            user_int: int = int(user_str)
+            return user_int
+        except ValueError:
+            self.show('Solo se permiten números')
+            return self.read_int(msg)
+
+    def read_int_range(self, valids: List[int], msg: str = '') -> int:
+        user_int: int = self.read_int(msg)
+        if(not user_int in valids):
+            print('valores permitidos: ', valids)
+            return self.read_int_range(valids, msg)
+        return user_int
