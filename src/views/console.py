2a3a2e85dccc4080d8939b562fd49ac02b3944a2
victor <hugohg34@gmail.com>
2021-08-07 01:28:13 +0200

    console functional

--- src/views/console.py
+++ src/views/console.py
@@ -1,28 +1,34 @@
 from typing import List
 
-class Console:
-    def show(self, msg: str) -> None:
-        print(msg)
-            
-    def read_str(self, msg: str = '') -> str:
-        while True:
-            user_input: str = input(msg + ': ')
-            if user_input:
-                break
-        return user_input
+ONLY_NUMBERS_ARE_ALLOWED: str = 'Only numbers are allowed'
+ALLOWED_VALUES: str = 'allowed values: '
 
-    def read_int(self, msg: str = '') -> int:
-        user_str: str = self.read_str(msg)
-        try:
-            user_int: int = int(user_str)
-            return user_int
-        except ValueError:
-            self.show('Solo se permiten números')
-            return self.read_int(msg)
 
-    def read_int_range(self, valids: List[int], msg: str = '') -> int:
-        user_int: int = self.read_int(msg)
-        if(not user_int in valids):
-            print('valores permitidos: ', valids)
-            return self.read_int_range(valids, msg)
+def show(msg: str) -> None:
+    print(msg)
+
+
+def read_str(msg: str = '') -> str:
+    while True:
+        user_input: str = input(msg + ': ')
+        if user_input:
+            break
+    return user_input
+
+
+def read_int(msg: str = '') -> int:
+    user_str: str = read_str(msg)
+    try:
+        user_int: int = int(user_str)
         return user_int
+    except ValueError:
+        show(ONLY_NUMBERS_ARE_ALLOWED)
+        return read_int(msg)
+
+
+def read_int_range(valids: List[int], msg: str = '') -> int:
+    user_int: int = read_int(msg)
+    if user_int not in valids:
+        print(ALLOWED_VALUES, valids)
+        return read_int_range(valids, msg)
+    return user_int
