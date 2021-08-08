0a0f6ef055bed9b3f1574902d453a132b27fac5d
Víctor Ramírez de la Corte <virako.9@gmail.com>
2021-08-08 12:50:45 +0200

    Add test for match and console

---
+++ tests/models/test_match.py
@@ -0,0 +1,32 @@
+import unittest
+
+from src.models import Dice, Match
+from src.types import Color
+
+
+class MatchTest(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.match = Match()
+
+    def test_initial_match(self) -> None:
+        self.assertFalse(self.match.games)
+        self.assertFalse(self.match.goal)
+        self.assertEqual(len(self.match.turn.players), 2)
+
+    def test_is_goal_false(self) -> None:
+        self.match.goal = 3
+        self.assertFalse(self.match.is_goal())
+
+    def test_is_goal_true(self) -> None:
+        self.match.goal = 3
+        self.match.turn.players[0].score = 3
+        self.assertTrue(self.match.is_goal())
+
+    def test_first_roll(self) -> None:
+        first_roll = self.match.first_roll()
+
+        self.assertTrue(Color.BLACK in first_roll.keys())
+        self.assertTrue(Color.RED in first_roll.keys())
+        for value in first_roll.values():
+            self.assertIsInstance(value, Dice)
---
+++ tests/views/test_console.py
@@ -0,0 +1,59 @@
+import random
+import string
+import unittest
+from unittest.mock import patch
+
+from src.views import console
+
+
+class ConsoleTestSuite(unittest.TestCase):
+
+    @classmethod
+    def gen_random_text(cls, length: int = 10) -> str:
+        return ''.join(random.choices(string.printable, k=length))
+
+    def test_show(self) -> None:
+        random_text = self.gen_random_text(20)
+        with patch('builtins.print') as mock:
+            console.show(random_text)
+            mock.assert_called_once_with(random_text)
+
+    def test_read_str(self) -> None:
+        empty_text = ''
+        random_text = self.gen_random_text(5)
+
+        with patch('builtins.input') as mock:
+            mock.side_effect = [empty_text, random_text]
+            result = console.read_str()
+            self.assertEqual(result, random_text)
+            self.assertEqual(mock.call_count, 2)
+
+    def test_read_int(self) -> None:
+        bad_input = 'bad'
+        good_input = '42'
+
+        with (
+            patch('builtins.input') as mock,
+            patch.object(console, 'show') as mock_show
+        ):
+            mock.side_effect = [bad_input, good_input]
+            result = console.read_int()
+            self.assertEqual(result, 42)
+            self.assertEqual(mock.call_count, 2)
+            mock_show.assert_called_once_with(console.ONLY_NUMBERS_ARE_ALLOWED)
+
+    def test_read_int_range(self) -> None:
+        valids = [1, 2]
+        bad_input = '4'
+        good_input = '2'
+
+        with (
+            patch('builtins.input') as mock_input,
+            patch('builtins.print') as mock_print
+        ):
+
+            mock_input.side_effect = [bad_input, good_input]
+            result = console.read_int_range(valids=valids)
+            self.assertEqual(result, 2)
+            self.assertEqual(mock_input.call_count, 2)
+            mock_print.assert_called_once_with(console.ALLOWED_VALUES, valids)
