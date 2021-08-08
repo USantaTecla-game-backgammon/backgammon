531c2a5c60c92bd0f34d3e887661c30064d7e54f
victor <hugohg34@gmail.com>
2021-08-08 06:44:32 +0200

    fix inicialización colores he implementando metodos

--- src/models/board.py
+++ src/models/board.py
@@ -1,58 +1,63 @@
 from typing import Dict
+from typing import Final
 from src.types.position import Position
 from src.types.color import Color
 
 
 class Board:
+    NUM_PICES_PER_COLOR: Final = 10;
+
     def __init__(self) -> None:
         self.positions: Dict[int, Dict[str, int]] = {}
         self.reset()
 
     def is_all_pieces_off_board(self, color: Color) -> bool:
-        raise NotImplementedError
+        return self.position_of_color(Position.OFF_BOARD, color) == self.NUM_PICES_PER_COLOR
 
     def is_any_piece_off_board(self, color: Color) -> bool:
-        raise NotImplementedError
+        return self.position_of_color(Position.OFF_BOARD, color) == 0;
 
     def is_any_piece_at_first_square(self, color: Color) -> bool:
         raise NotImplementedError
 
     def is_any_piece_in_bar(self, color: Color) -> bool:
-        raise NotImplementedError
+        return self.position_of_color(Position.BAR, color) > 0
 
     def is_all_pieces_last_square(self, color: Color) -> bool:
         raise NotImplementedError
 
     def move_pice(self, position_from: Position, position_to: Position, color: Color) -> None:
-        raise NotImplementedError
-        #self.positions.update(position_from)
+        pos_from_absolute = self.position_of_color(position_from, color)
+        pos_to_absolute = self.position_of_color(position_to, color)
+        self.positions[pos_from_absolute][color.name] -= 1;
+        self.positions[pos_to_absolute][color.name] += 1;
 
     def reset(self) -> None:
         self.positions.clear()
-        self.__update_position(Position.TWENTY_FOUR, 0, 2)
+        self.__update_position(Position.TWENTY_FOUR, 2, 0)
         self.__update_position(Position.TWENTY_THREE, 0,0)
         self.__update_position(Position.TWENTY_TWO, 0,0)
         self.__update_position(Position.TWENTY_ONE, 0,0)
         self.__update_position(Position.TWENTY, 0,0)
-        self.__update_position(Position.NINETEEN, 5, 0)
+        self.__update_position(Position.NINETEEN, 0, 5)
         self.__update_position(Position.EIGHTEEN, 0,0)
-        self.__update_position(Position.SEVENTEEN, 3, 0)
+        self.__update_position(Position.SEVENTEEN, 0, 3)
         self.__update_position(Position.SIXTEEN, 0,0)
         self.__update_position(Position.FIFTEEN, 0,0)
         self.__update_position(Position.FOURTEEN, 0,0)
-        self.__update_position(Position.THIRTEEN, 0, 5)
-        self.__update_position(Position.TWELVE, 5, 0)
+        self.__update_position(Position.THIRTEEN, 5, 0)
+        self.__update_position(Position.TWELVE, 0, 5)
         self.__update_position(Position.ELEVEN, 0,0)
         self.__update_position(Position.TEN, 0,0)
         self.__update_position(Position.NINE, 0,0)
-        self.__update_position(Position.EIGHT, 0, 3)
+        self.__update_position(Position.EIGHT, 3, 0)
         self.__update_position(Position.SEVEN, 0,0)
-        self.__update_position(Position.SIX, 0, 5)
+        self.__update_position(Position.SIX, 5, 0)
         self.__update_position(Position.FIVE, 0,0)
         self.__update_position(Position.FOUR, 0,0)
         self.__update_position(Position.THREE, 0,0)
         self.__update_position(Position.TWO, 0,0)        
-        self.__update_position(Position.ONE, 2, 0)
+        self.__update_position(Position.ONE, 0, 2)
 
         self.__update_position(Position.BAR, 0, 0)
         self.__update_position(Position.OFF_BOARD, 0, 0)
