b561f18b2aa6fcc4390458c757da8857af33a3aa
Víctor Ramírez de la Corte <virako.9@gmail.com>
2021-08-14 22:30:27 +0200

    remove inyection view factory in backgammon. Closed #39

--- main.py
+++ main.py
@@ -1,27 +1,13 @@
 import sys
 
 from src.backgammon import Backgammon
-from src.views.console.console_view_factory import ConsoleViewFactory
-from src.views.gui.gui_view_factory import GUIViewFactory
-from src.views.view_factory import ViewFactory
 
 
-factories = {
-    'default': ConsoleViewFactory(),
-    'console': ConsoleViewFactory(),
-    'gui': GUIViewFactory(),
-}
-
-
-def read_settings_from_arguments() -> ViewFactory:
+def read_settings_from_arguments() -> str:
     arguments = sys.argv[1:]
-    key = arguments[0] if arguments else 'default'
-
-    if key in factories.keys():
-        return factories.get(key)
-    return factories.get('default')
+    return arguments[0] if arguments else ''
 
 
 if __name__ == '__main__':
-    view_factory = read_settings_from_arguments()
-    Backgammon(view_factory).play()
+    SETTINGS = read_settings_from_arguments()
+    Backgammon(SETTINGS).play()
--- src/backgammon.py
+++ src/backgammon.py
@@ -1,16 +1,26 @@
 from src.controllers import PlayController, ResumeController, StartController
 from src.models import Match
 from src.views.view_factory import ViewFactory
+from src.views.console.console_view_factory import ConsoleViewFactory
+from src.views.gui.gui_view_factory import GUIViewFactory
 
 
 class Backgammon:
-    def __init__(self, view_factory: ViewFactory) -> None:
+    VIEW_FACTORIES: dict[str, ViewFactory] = {
+        'console': ConsoleViewFactory(),
+        'gui': GUIViewFactory(),
+    }
+
+    def __init__(self, settings: str) -> None:
         self.match = Match()
-        self.view_factory = view_factory
+        self.view_factory = self.create_factory(settings)
 
         self.start_controller = StartController(self.match, self.view_factory)
         self.play_controller = PlayController(self.match, self.view_factory)
         self.resume_controller = ResumeController(self.match, self.view_factory)
+
+    def create_factory(self, settings: str) -> ViewFactory:
+        return self.VIEW_FACTORIES.get(settings, ConsoleViewFactory())
 
     def play(self) -> None:
         while True:
