74957079a725626f8a83a9edc35b725a5583b958
victor <hugohg34@gmail.com>
2021-08-15 21:14:16 +0200

    Implement boardSerializer

---
+++ src/serializers/board_serializer.py
@@ -0,0 +1,28 @@
+from typing import Any
+from typing import Final
+import json
+from src.models.board import Board
+from src.types.color import Color
+from src.types.position import Position
+
+
+class BoardSerializer(Board):
+    board: Board
+ 
+    def __init__(self, board: Board) -> None:
+        self.board = board
+
+    def serialize(self) -> str:
+        available_colors: list[Color] = [Color.BLACK, Color.RED]
+        board_dic: dict[str, dict[str, dict[str, int]]]= {}
+        for sense_color in available_colors:
+            board_dic[sense_color.name] = {}
+            for position in Position:
+                board_dic[sense_color.name][position.name] = {}
+                pices_in_position: list[Color] = self.board.get_pieces(sense_color, position)
+                num_of_color_dic = {}
+                for pice_color in available_colors:
+                    num_of_color_dic[pice_color.name] = pices_in_position.count(pice_color)
+                board_dic[sense_color.name][position.name].update(num_of_color_dic)
+
+        return json.dumps(board_dic, indent=4)
