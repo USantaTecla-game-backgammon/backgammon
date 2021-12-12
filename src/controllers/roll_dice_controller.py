130f410d8032aed6878f7ba1dc2a882c68450477
Víctor Ramírez de la Corte <virako.9@gmail.com>
2021-12-12 19:12:25 +0100

    Create RollDiceController for execute first_roll function

---
+++ src/controllers/roll_dice_controller.py
@@ -0,0 +1,33 @@
+from typing import Optional
+
+from src.controllers.controller import Controller
+from src.models import Dice, Match
+from src.views.view_factory import ViewFactory
+from src.types import Color
+
+
+class RollDiceController(Controller):
+
+    def __init__(self, match: Match, view_factory: ViewFactory) -> None:
+        super().__init__(match, view_factory)
+        self.view = view_factory.create_match_view()
+
+    def __call__(self) -> None:
+        self.first_roll()
+
+    def first_roll(self) -> None:
+        dices: dict[Color, Dice] = {}
+        winner_color: Optional[Color] = None
+
+        while not winner_color:
+            dices = self.match.throw_first_dices()
+
+            if dices[Color.BLACK] > dices[Color.RED]:
+                winner_color = Color.BLACK
+            elif dices[Color.BLACK] < dices[Color.RED]:
+                winner_color = Color.RED
+
+            self.view.show_dices(dices)
+
+        self.match.change_turn(winner_color)
+        self.match.first_roll = list(dices.values())
--- src/controllers/start_controller.py
+++ src/controllers/start_controller.py
@@ -1,9 +1,7 @@
-from typing import Optional
-
 from src.controllers.controller import Controller
-from src.models import Dice, Match
+from src.controllers.roll_dice_controller import RollDiceController
+from src.models import Match
 from src.views.view_factory import ViewFactory
-from src.types import Color
 
 
 class StartController(Controller):
@@ -11,28 +9,12 @@ class StartController(Controller):
     def __init__(self, match: Match, view_factory: ViewFactory) -> None:
         super().__init__(match, view_factory)
         self.view = view_factory.create_match_view()
+        self.roll_dice_controller = RollDiceController(match, view_factory)
 
     def __call__(self) -> None:
         self.configure_goal()
-        self.first_roll()
+        self.roll_dice_controller()
 
     def configure_goal(self) -> None:
         self.view.show_title()
         self.match.goal = self.view.read_goal()
-
-    def first_roll(self) -> None:
-        dices: dict[Color, Dice] = {}
-        winner_color: Optional[Color] = None
-
-        while not winner_color:
-            dices = self.match.throw_first_dices()
-
-            if dices[Color.BLACK] > dices[Color.RED]:
-                winner_color = Color.BLACK
-            elif dices[Color.BLACK] < dices[Color.RED]:
-                winner_color = Color.RED
-
-            self.view.show_dices(dices)
-
-        self.match.change_turn(winner_color)
-        self.match.first_roll = list(dices.values())
