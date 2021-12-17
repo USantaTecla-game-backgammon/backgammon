7dbdf85c212f25d2d219531b914dffe4f2201d25
Víctor Ramírez de la Corte <virako.9@gmail.com>
2021-12-17 18:08:52 +0100

    Refactor SaveController __call__

--- src/controllers/save_controller.py
+++ src/controllers/save_controller.py
@@ -1,16 +1,19 @@
 import pickle
 from pathlib import Path
+from typing import Final
 
 from src.models.match import Match
 
 
 class SaveController:
+    FOLDER: Final[str] = 'saved'
+
     def __init__(self, match: Match, filename: str) -> None:
         self.match = match
         self.filename = filename
 
     def __call__(self) -> None:
-        folder = Path('saved')
+        folder = Path(self.FOLDER)
         folder.mkdir(exist_ok=True)
-        with open(f'saved/{self.filename}', 'wb') as f:
+        with open(f'{self.FOLDER}/{self.filename}', 'wb') as f:
             f.write(pickle.dumps(self.match.__dict__))
