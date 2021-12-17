f556bddafcbf1fb0550190b8839174c4d95dfbde
Víctor Ramírez de la Corte <virako.9@gmail.com>
2021-12-17 18:45:13 +0100

    Create new broken test, because user should give a filename

--- tests/controllers/test_save_controller.py
+++ tests/controllers/test_save_controller.py
@@ -1,9 +1,12 @@
 import unittest
+from unittest.mock import patch
 
 from src.models.match import Match
 from src.models.move import Move
 from src.controllers.save_controller import SaveController
 from src.types.position import Position
+from src.views.console.match_view import MatchView
+from src.views.console.console_view_factory import ConsoleViewFactory
 
 
 class SaveControllerTest(unittest.TestCase):
@@ -42,3 +45,13 @@ class SaveControllerTest(unittest.TestCase):
             text1 = file1.read()
             text2 = file2.read()
         self.assertNotEqual(text1, text2)
+
+    def test_save_match_with_name_given_by_player(self) -> None:
+        match = Match()
+        filename = 'save.pickle'
+        with patch.object(MatchView, 'read_filename', return_value=filename) as mock:
+            save_controller = SaveController(match, ConsoleViewFactory())
+            save_controller()
+
+        self.assertEqual(mock.call_count, 1)
+        self.assertEqual(filename, save_controller.filename)
