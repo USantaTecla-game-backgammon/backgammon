973e63d629e5e65084cf23f09f2161fd46717142
Víctor Ramírez de la Corte <virako.9@gmail.com>
2021-12-17 17:57:56 +0100

    Create test for check that save different match status

--- src/controllers/save_controller.py
+++ src/controllers/save_controller.py
@@ -1,3 +1,4 @@
+import pickle
 from pathlib import Path
 
 from src.models.match import Match
@@ -5,10 +6,11 @@ from src.models.match import Match
 
 class SaveController:
     def __init__(self, match: Match, filename: str) -> None:
+        self.match = match
         self.filename = filename
 
     def __call__(self) -> None:
         folder = Path('saved')
         folder.mkdir(exist_ok=True)
-        with open(f'saved/{self.filename}', 'w') as f:
-            f.write('')
+        with open(f'saved/{self.filename}', 'wb') as f:
+            f.write(pickle.dumps(self.match.__dict__))
--- tests/controllers/test_save_controller.py
+++ tests/controllers/test_save_controller.py
@@ -2,7 +2,9 @@ import unittest
 from pathlib import Path
 
 from src.models.match import Match
+from src.models.move import Move
 from src.controllers.save_controller import SaveController
+from src.types.position import Position
 
 
 class SaveControllerTest(unittest.TestCase):
@@ -18,3 +20,22 @@ class SaveControllerTest(unittest.TestCase):
         self.save_controller = SaveController(self.match, 'save2.pickle')
         self.save_controller()
         self.assertTrue(Path('saved/save2.pickle').is_file())
+
+    def test_save_with_different_state_are_different(self) -> None:
+        self.match = Match()
+        self.save_controller = SaveController(self.match, 'state1.pickle')
+        self.save_controller()
+
+        self.match = Match()
+        self.match.last_game.possible_moves = [4]
+        self.match.last_game.move_piece(Move(position_from=Position.TWENTY_FOUR, dice_value=4))
+        self.save_controller = SaveController(self.match, 'state2.pickle')
+        self.save_controller()
+
+        with (
+            open('saved/state1.pickle', 'rb') as file1,
+            open('saved/state2.pickle', 'rb') as file2
+        ):
+            text1 = file1.read()
+            text2 = file2.read()
+        self.assertNotEqual(text1, text2)
