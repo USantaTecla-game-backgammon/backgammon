d9f815575441ad7baf0ea435cc28b61f305f0a32
Víctor Ramírez de la Corte <virako.9@gmail.com>
2021-12-17 18:31:10 +0100

    Refactor test: clean self, delete Path dependency and extract filepath instead path + filename

--- tests/controllers/test_save_controller.py
+++ tests/controllers/test_save_controller.py
@@ -1,5 +1,4 @@
 import unittest
-from pathlib import Path
 
 from src.models.match import Match
 from src.models.move import Move
@@ -10,31 +9,35 @@ from src.types.position import Position
 class SaveControllerTest(unittest.TestCase):
 
     def test_save_complete_match(self) -> None:
-        self.match = Match()
-        self.save_controller = SaveController(self.match, 'save.pickle')
-        self.save_controller()
-        self.assertTrue(Path('saved/save.pickle').is_file())
+        match = Match()
+        filename = 'save.pickle'
+        save_controller = SaveController(match, filename)
+        save_controller()
+        self.assertTrue(save_controller.filepath.is_file())
 
     def test_save_complete_match_with_different_name(self) -> None:
-        self.match = Match()
-        self.save_controller = SaveController(self.match, 'save2.pickle')
-        self.save_controller()
-        self.assertTrue(Path('saved/save2.pickle').is_file())
+        match = Match()
+        filename = 'save2.pickle'
+        save_controller = SaveController(match, filename)
+        save_controller()
+        self.assertTrue(save_controller.filepath.is_file())
 
     def test_save_with_different_state_are_different(self) -> None:
-        self.match = Match()
-        self.save_controller = SaveController(self.match, 'state1.pickle')
-        self.save_controller()
+        match = Match()
+        filename = 'state1.pickle'
+        save_controller = SaveController(match, filename)
+        save_controller()
 
-        self.match = Match()
-        self.match.last_game.possible_moves = [4]
-        self.match.last_game.move_piece(Move(position_from=Position.TWENTY_FOUR, dice_value=4))
-        self.save_controller = SaveController(self.match, 'state2.pickle')
-        self.save_controller()
+        match = Match()
+        match.last_game.possible_moves = [4]
+        match.last_game.move_piece(Move(position_from=Position.TWENTY_FOUR, dice_value=4))
+        filename2 = 'state2.pickle'
+        save_controller2 = SaveController(match, filename2)
+        save_controller2()
 
         with (
-            open('saved/state1.pickle', 'rb') as file1,
-            open('saved/state2.pickle', 'rb') as file2
+            save_controller.filepath.open(mode='rb') as file1,
+            save_controller2.filepath.open(mode='rb') as file2,
         ):
             text1 = file1.read()
             text2 = file2.read()
