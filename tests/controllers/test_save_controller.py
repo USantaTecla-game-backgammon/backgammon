c0b39f670b15670e2f5b1748e35798de2fa24eb6
Víctor Ramírez de la Corte <virako.9@gmail.com>
2021-12-17 15:56:56 +0100

    New test created for save feature

---
+++ tests/controllers/test_save_controller.py
@@ -0,0 +1,13 @@
+import unittest
+from pathlib import Path
+
+from src.models.match import Match
+
+
+class SaveControllerTest(unittest.TestCase):
+
+    def test_save_complete_match(self) -> None:
+        self.match = Match()
+        self.save_controller = SaveController(self.match, 'save.pickle')
+        self.save_controller()
+        self.assertTrue(Path('saved/save.pickle').is_file())
